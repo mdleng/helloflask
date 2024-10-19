086edadf8117983c43edf5fad7cb8a19d57a1688
mdleng <47358420+mdleng@users.noreply.github.com>
2024-10-19 14:44:19 +0100

    Update EI_http_http_flask.py

--- EI_http_http_flask.py
+++ EI_http_http_flask.py
@@ -1,12 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
+global last_data
+last_data={
+        'id': 0
+    }
 @app.route('/sensor', methods=['POST'])
 def sensor():
     # Get the JSON data from the request
     data = request.get_json()
+    last_data=data
     # Print the data to the console
     print(data)
     # Return a success message
     return 'JSON received!'
+
+@app.route('/data')
+def data():
+    return jsonify(data)
